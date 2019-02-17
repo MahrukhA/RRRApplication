6d7cb2a2a81f013a4fb021ab892cfde1e0ae8033
Stephen <stephen.lerminiaux@gmail.com>
2019-02-17 02:41:21 -0600

    started abstract classes for observer classes

--- RRR/listings/observer.py
+++ RRR/listings/observer.py
@@ -30,3 +30,25 @@ class Observer:
     def update(self):
         """Observer updates by pulling data from Subject."""
         pass
+
+
+class ListingData(Subject):
+    # Concrete Subject
+    def __init__(self):
+        self._observers = []
+
+    def register(self, observer):
+        if observer not in self._observers:
+            self._observers.append(observer)
+        else:
+            print('Failed to add: {}'.format(observer))
+
+    def remove(self, observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            print('Failed to remove: {}'.format(observer))
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update()
