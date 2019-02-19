bb767208a927ddc7ef30a1e9540a4d7abc830dad
Mathew Dorish <mathewdorish@hotmail.com>
2019-02-19 15:27:14 -0600

    added gmail settings

--- RRR/RRR/settings.py
+++ RRR/RRR/settings.py
@@ -11,6 +11,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+#FOR GMAIL
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'RRRentables@gmail.com'
+EMAIL_HOST_PASSWORD = 'coconutRRR'
+EMAIL_PORT = 587
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
