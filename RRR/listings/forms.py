c11b86427e42f627e791478a47ae7c0fe2cca4b2
Mahrukh Aslam <itsmahrukh@hotmail.com>
2019-03-03 19:29:35 -0600

    image upload for edit page

--- RRR/listings/forms.py
+++ RRR/listings/forms.py
@@ -3,6 +3,10 @@ from .models import Listing
 
 class ListingForm(forms.ModelForm):
 	photo_1 = forms.FileField(required=False)
+	photo_2 = forms.FileField(required=False)
+	photo_3 = forms.FileField(required=False)
+	photo_4 = forms.FileField(required=False)
+	photo_5 = forms.FileField(required=False)
 
 	class Meta:
 		model = Listing
