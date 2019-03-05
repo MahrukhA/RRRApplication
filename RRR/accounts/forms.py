931452440ec44df067af056ba3f3c22a54eb93a2
Stephen <stephen.lerminiaux@gmail.com>
2019-03-05 11:05:37 -0600

    Small changes

--- RRR/accounts/forms.py
+++ RRR/accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegisterForm(UserCreationForm):
+    """Form used to create a user in the built-in User table"""
     first_name = forms.CharField(max_length=30, required=True, help_text='Required')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
